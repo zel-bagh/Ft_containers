d3132f5205bc12f80d57894f0b8b99556b87e03c
Zakaria El baghazaoui <zel-bagh@e1r4p4.1337.ma>
2022-07-28 16:13:30 +0100

    inserting..

--- map/RedBlackTree.hpp
+++ map/RedBlackTree.hpp
@@ -8,20 +8,17 @@ class RBT
     public:
         typedef typename Alloc::reference   reference;
         typedef typename Alloc::pointer     pointer;
-    private:
-        RBTNode* root_node;
+        typedef typename Alloc::_Tp         value_type;
     public:
         RBT(void) {root_node = NULL;}
-        RBT(const RBT& obj) {*this = obj;}
-        ~RBT(void) {}
+        RBT(const RBT& obj) {*this = obj;} // should copy
+        ~RBT(void) {} /// should destruct all nodes
     public:
         const RBT& operator=(const RBT& obj)
         {
             (void)obj;
             return *this;
         }
-    public:
-        std::pair<typename RBT::iterator, bool> insert(const std::pair<const Key, T>& pair);
     public:
         class RBTNode
         {
@@ -37,7 +34,7 @@ class RBT
                 {
                     Alloc allocator;
                     this->pair = allocator.allocate(1);
-                    this->pair[0] = pair; 
+                    this->pair[0] = pair;
                 }
                 RBTNode(const RBTNode& obj) {*this = obj;}
                 ~RBTNode()
@@ -47,7 +44,7 @@ class RBT
             public:
                 const RBTNode& operator=(const RBTNode& obj)
                 {
-                    (void)obj;
+                    (void)obj; //////////
                     return *this;
                 }
         };
@@ -58,6 +55,7 @@ class RBT
                 RBTNode* node;
             public:
                 iterator(void) {node = NULL;}
+                iterator(const RBTNode& obj){node = &obj};
                 iterator(const iterator& obj) {*this = obj;}
                 ~iterator(void){}
             public:
@@ -69,13 +67,62 @@ class RBT
                 reference   operator->(void) {return (*(node->pair));}
                 reference   operator*(void) {return (*(node->pair));}
         };
-
+    public:
+        std::pair<iterator, bool> insert(const std::pair<const Key, T>& pair);
+    private:
+        RBTNode* root_node;
 };
 
 template <class Key, class T, class Key_Compare, class Alloc>
 std::pair<typename RBT<Key, T, Key_Compare, Alloc>::iterator, bool> RBT<Key, T, Key_Compare, Alloc>::insert(const std::pair<const Key, T>& pair)
 {
-    
+    bool       r;
+    value_type pr;
+    RBTNode    *node;
+    Key_Compare compare;
+    iterator    it;
+    std::pair<iterator, bool> return_value;
+
+    pr.first = pair.first;
+    pr.second = pair.second;
+    if (root_node == NULL) // create root node
+    {
+        root_node = new RBTNode(pr);
+        root_node->is_black = 1;
+        root_node->is_root = 1;
+        root_node->left_child = NULL;
+        root_node->right_child = NULL;
+        root_node->parent = NULL;
+    }
+    else
+    {
+        node = root_node;
+        while (1)
+        {
+            r = compare(pr.first, node->pair->first);
+            if (compare(pr.first, node->pair->first) == 1)
+            {
+                if (node->left_child == NULL)
+                {
+                    node->left_child = new RBTNode(pr);
+                    break ;
+                }
+                    node = node->left_child;
+            }
+            else if (compare(node->pair->first, pr.first) == 1)
+            {
+                if (node->right_child == NULL)
+                {
+                    node->right_child = new RBTNode(pr);
+                    break ;
+                }
+                    node = node->right_child;  
+            }
+            else
+                return (std::pair<iterator, bool> (iterator(*node), 0));
+        }
+        
+    }
 } 
 
 
--- map/test.cpp
+++ map/test.cpp
@@ -2,14 +2,12 @@
 #include<iostream>
 #include<map>
 #include"test.hpp"
-#include"RedBlackTree.hpp"
+// #include"RedBlackTree.hpp"
 
 
 int main()
 {
     std::map<int, int> maap;
-    std::pair<const int, int>* pair;
-    std::map<int, int>::iterator it(maap.begin());
 
-    
+
 }
